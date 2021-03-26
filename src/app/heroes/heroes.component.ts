3e4d3e5059dec4ae636a52c073010266f4ca65bd
Kevin Daniel Rosas Montoya <201821214@tese.edu.mx>
2021-03-25 21:34:55 -0600

    ListaDinamica

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../Hero';
+import {HEROES} from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero: Hero={
-    id: 1,
-    name:'Spiderman',
-    edad: 23,
-    poder: 'Poderes de aracnido',
-    identidad: 'Peter Parker',
-    universo: 'Marvel'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  OnSelected(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero}from "./Hero";
+
+export const HEROES: Hero[] = [
+{id: 11, name: "Superman", edad: 30, poder: "Criptoniano", identidad: "Clark Kent", universo: "DC"},
+{id: 12, name: "Batman", edad: 30, poder: "Dinero", identidad: "Bruce Wayne", universo: "DC"},
+{id: 13, name: "Hulk", edad: 40, poder: "Fuerza", identidad: "Bruce Banner", universo: "Marvel"},
+{id: 14, name: "Ironman", edad: 25, poder: "Dinero", identidad: "Tony Stark", universo: "Marvel"},
+{id: 15, name: "Thor", edad: 1000, poder: "Semidios", identidad: "Thor", universo: "Marvel"},
+{id: 16, name: "Spiderman", edad: 20, poder: "Aracnidos", identidad: "Peter Parker", universo: "Marvel"},
+{id: 17, name: "Black Widow", edad: 28, poder: "Ninja", identidad: "Natasha Romanoff", universo: "Marvel"},
+{id: 18, name: "Flash", edad: 28, poder: "Super Velocidad", identidad: "Barry Allen", universo: "DC"},
+{id: 19, name: "Bruja Escarlata", edad: 35, poder: "Controla la realidad", identidad: "Wanda Maximoff", universo: "Marvel"},
+{id: 20, name: "Wonder Woman", edad: 30, poder: "Amazona", identidad: "Diana", universo: "DC"}
+];
+
+
